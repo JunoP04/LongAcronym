642ca7157777fba2c67ae55c0875bf3e2febe746
EfeKaracar <efekaracar@gmail.com>
2019-09-29 18:35:42 -0400

    Stuff
    Stuff

--- DungeonGame/Assets/Scripts/resourceTracking.cs
+++ DungeonGame/Assets/Scripts/resourceTracking.cs
@@ -6,10 +6,12 @@ public class resourceTracking : MonoBehaviour
 {
 
     private int woodAmount;
+    private int goldAmount;
 
     // Update is called once per frame
     void Update()
     {
-        
+        woodAmount = +1;
+        goldAmount = +1;
     }
 }
